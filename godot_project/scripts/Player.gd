c772440d45641638694053b62f1bfc9d0036d92e
mucrow <120820315+mucrow@users.noreply.github.com>
2024-08-10 19:29:11 -0400

    Normalize move inputs

--- godot_project/scripts/Player.gd
+++ godot_project/scripts/Player.gd
@@ -11,6 +11,10 @@ func _physics_process(_delta):
     pass
 
   var move_input = Input.get_vector('move_left', 'move_right', 'move_up', 'move_down')
+  if move_input.length() < 0.9:
+    move_input = Vector2.ZERO
+  else:
+    move_input = move_input.normalized()
 
   if move_input.length() > 0:
     var target_velocity = move_input * SPEED
